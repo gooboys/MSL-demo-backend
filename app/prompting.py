db0cedfef4aba270c6e8a4ef6ce6f118b0c9e831
Thierry Juang <thierry.tj007@gmail.com>
2025-08-21 02:07:07 -0400

    fixed

--- app/prompting.py
+++ app/prompting.py
@@ -1,3 +1,12 @@
 def attach_prompts(data):
+  remove = ["KOL Full Name", 
+            "Therapeutic Area", 
+            "Product Discussed", 
+            "MSL / Submitter Name", 
+            "Company Sponsored Research Details",
+            "Insight Category",
+            "US: Unsolicited Request for Information"
+            ]
+  for i in remove:del data[i]
   
-  return
+  return data
