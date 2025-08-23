9607904252de928d14683e7ccd40cdcd7391cf2e
Thierry Juang <thierry.tj007@gmail.com>
2025-08-22 23:42:42 -0400

    error fixed

--- app/prompting.py
+++ app/prompting.py
@@ -21,10 +21,10 @@ def attach_final_prompt(data, cat):
 #{ 0: education_communication_prompts, 1: clinical_practice_prompts, 2: competitive_intelligence_prompts}
 def attach_education_prompts(content, prompt_number, records):
   if prompt_number == 1:
-    response = attach_second_prompt(content, prompt_number, 0)
+    response = attach_second_prompt(content, 0)
   elif prompt_number == 2:
-    response = attach_third_prompt(content, prompt_number, 0)
+    response = attach_third_prompt(content, 0)
   elif prompt_number == 3:
-    response = attach_final_prompt(content, prompt_number, 0)
+    response = attach_final_prompt(content, 0)
   rec = json.dumps(records, indent=2)
   return response+rec
